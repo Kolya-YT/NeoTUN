69494c3ff4c5c28fa05942a41ea06827aa41ae9e
KolyaYT <KolyaYT@DESKTOP-BMSEPC3>
2025-12-01 21:22:47 +0300

    Explicitly disable shrinkResources

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -52,10 +52,7 @@ android {
         release {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isShrinkResources = false
         }
     }
 }
