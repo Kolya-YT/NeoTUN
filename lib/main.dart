d55bfb6cb2397400a0b63075c217bb8a8250c8d5
KolyaYT <KolyaYT@DESKTOP-BMSEPC3>
2025-12-02 03:42:40 +0300

    Fix localization imports for v1.2.0-beta.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'l10n/app_localizations.dart';
 import 'screens/home_screen.dart';
 import 'screens/qr_scanner_screen.dart';
 import 'services/core_manager.dart';
