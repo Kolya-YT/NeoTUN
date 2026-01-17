dcff77191a8e6d202f11eb7a099dd9a5de8eea46
Kolya-YT <kolya.yt@example.com>
2026-01-17 17:54:50 +0300

    Fix Kotlin serialization plugin ID in Gradle build files

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("kotlinx-serialization") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
 }
 
 tasks.register("clean", Delete::class) {
