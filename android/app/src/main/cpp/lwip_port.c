4666113dac0759df45d469c5800dc3b76af31b28
Kolya-YT <kolya3985@email.com>
2026-03-23 02:14:42 +0300

    fix: lwip_port_rand use /dev/urandom instead of getrandom

--- android/app/src/main/cpp/lwip_port.c
+++ android/app/src/main/cpp/lwip_port.c
@@ -1,8 +1,13 @@
 #include <stdint.h>
-#include <sys/random.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 uint32_t lwip_port_rand(void) {
-    uint32_t r;
-    getrandom(&r, sizeof(r), 0);
+    uint32_t r = 0;
+    int fd = open("/dev/urandom", O_RDONLY);
+    if (fd >= 0) {
+        read(fd, &r, sizeof(r));
+        close(fd);
+    }
     return r;
 }
